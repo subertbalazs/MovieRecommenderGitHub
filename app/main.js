fc78c304e26bf6771ac465db2688501b308ff6c1
subertbalazs <balazs.subert@gmail.com>
2016-06-20 17:36:06 +0200

    Added registration form (ts component, html, css)

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_component_1 = require('./app.component');
+var registration_form_component_1 = require('./registration-form/registration-form.component');
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+platform_browser_dynamic_1.bootstrap(registration_form_component_1.RegistrationFieldComponent);
 //# sourceMappingURL=main.js.map
---
+++ app/registration-form/registration-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'reg',
+    templateUrl: 'app/registration-form/registration-form.component.html'
+})
+
+export class RegistrationFieldComponent{
+
+}
