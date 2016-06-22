cb82a5f60cbc107aa73e4dab707978b4e9b472b1
Herczku Misi <kmisku@gmail.com>
2016-06-22 09:32:05 +0200

    #49 Add Profile component

---
+++ app/profile/profile.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by herczkumihalybalazs on 2016.06.22..
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'profile',
+    templateUrl: 'app/profile/profile.component.html'
+})
+
+export class ProfileComponent {
+
+}
