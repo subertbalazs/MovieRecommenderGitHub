e51e323c331f92f66502eecc32fb4c15747a91f6
Kozma Balazs <kozma.balazs1992@gmail.com>
2016-07-01 11:53:14 +0200

    #53 password validation (not yet working)

--- app/registration-form/registration-form.component.js
+++ app/registration-form/registration-form.component.js
@@ -12,6 +12,12 @@ var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var common_1 = require("@angular/common");
 var custom_validators_1 = require('./custom-validators');
+function equalValidator(_a) {
+    var value = _a.value;
+    var _b = Object.keys(value || {}), first = _b[0], rest = _b.slice(1);
+    var valid = rest.every(function (v) { return value[v] === value[first]; });
+    return valid ? null : { equal: true };
+}
 var RegistrationFieldComponent = (function () {
     function RegistrationFieldComponent(router, builder) {
         this.router = router;
@@ -21,15 +27,21 @@ var RegistrationFieldComponent = (function () {
         this.address = new common_1.Control('', common_1.Validators.maxLength(150));
         this.birthDate = new common_1.Control('', common_1.Validators.required);
         this.gender = new common_1.Control('', common_1.Validators.required);
-        /*this.password = new Control('', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(50), CustomValidators.passwordFormat]));
-         this.confirmPassword = new Control();*/
+        this.password = new common_1.Control('', common_1.Validators.compose([common_1.Validators.required, common_1.Validators.minLength(6), common_1.Validators.maxLength(50), custom_validators_1.CustomValidators.passwordFormat]));
+        this.confirmPassword = new common_1.Control();
         this.group = builder.group({
             firstName: this.firstName,
             lastName: this.lastName,
             nickName: this.nickName,
             gender: this.gender,
             birthDate: this.birthDate,
-            address: this.address
+            address: this.address,
+            password: this.password,
+            confirmPassword: this.confirmPassword,
+            'passwordsGroup': new common_1.ControlGroup({
+                'password': new common_1.Control(''),
+                'password2': new common_1.Control('')
+            }, {}, equalValidator)
         });
     }
     RegistrationFieldComponent.prototype.gotToProfileFromReg = function () {
@@ -40,6 +52,7 @@ var RegistrationFieldComponent = (function () {
     RegistrationFieldComponent = __decorate([
         core_1.Component({
             selector: 'reg',
+            directives: [common_1.FORM_DIRECTIVES],
             templateUrl: 'app/registration-form/registration-form.component.html',
             styleUrls: ['app/registration-form/registration-form.component.css']
         }), 
--- app/registration-form/registration-form.component.ts
+++ app/registration-form/registration-form.component.ts
@@ -1,10 +1,17 @@
 import { Component } from "@angular/core";
 import {Router} from "@angular/router";
-import { FormBuilder, Validators, ControlGroup, Control} from "@angular/common";
+import {Control, FormBuilder, Validators, ControlGroup, FORM_DIRECTIVES} from "@angular/common";
 import {CustomValidators} from './custom-validators';
+
+function equalValidator({value}: ControlGroup): {[key: string]: any} {
+    const [first, ...rest] = Object.keys(value || {});
+    const valid = rest.every(v => value[v] === value[first]);
+    return valid ? null : {equal: true};
+}
 
 @Component({
     selector: 'reg',
+    directives: [FORM_DIRECTIVES],
     templateUrl: 'app/registration-form/registration-form.component.html',
     styleUrls: ['app/registration-form/registration-form.component.css']
 })
@@ -27,18 +34,22 @@ export class RegistrationFieldComponent {
         this.address = new Control('', Validators.maxLength(150));
         this.birthDate = new Control('', Validators.required);
         this.gender = new Control('', Validators.required);
-        /*this.password = new Control('', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(50), CustomValidators.passwordFormat]));
-         this.confirmPassword = new Control();*/
+        this.password = new Control('', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(50), CustomValidators.passwordFormat]));
+        this.confirmPassword = new Control();
         this.group = builder.group({
             firstName: this.firstName,
             lastName: this.lastName,
             nickName: this.nickName,
             gender: this.gender,
             birthDate: this.birthDate,
-            address: this.address
-            /*password: this.password,
-             confirmPassword: this.confirmPassword*/
-        });
+            address: this.address,
+            password: this.password,
+            confirmPassword: this.confirmPassword,
+            'passwordsGroup': new ControlGroup({
+            'password': new Control(''),
+            'password2': new Control('')
+        }, {}, equalValidator)
+    });
     }
     gotToProfileFromReg() {
         this.router.navigate(['/profile']);
